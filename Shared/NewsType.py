bca924a72c5ac7d61d4011010c0cf3b8cb43b566
Dragos Slusariuc <Dragos.Slusariuc@centric.eu>
2019-03-11 17:42:58 +0200

    Added crawler

---
+++ Shared/NewsType.py
@@ -0,0 +1,54 @@
+
+
+newsType = dict()
+newsType['economic'] = ['capital',
+                        'leu',
+                        'leului',
+                        'fonduri',
+                        'fondurile',
+                        'pensie',
+                        'anaf',
+                        'loto']
+
+
+newsType['politica'] = ['demis',
+                        'vot',
+                        'votul',
+                        'cenzura',
+                        'salarii',
+                        'udmr',
+                        'psd',
+                        'pnl',
+                        'militari',
+                        'proteste',
+                        'protest',
+                        'dragnea']
+
+newsType['sport'] = ['medalie',
+                     'tir',
+                     '']
+
+newsType['accidente'] = ['murit',
+                         'moarte',
+                         'moartea',
+                         'doliu',
+                         'incendiu']
+
+
+newsType['infractiuni'] = ['mafia',
+                           'jocuri-de-noroc',
+                           'amenintare']
+
+newsType['meteo'] = ['vremea',
+                     'meteo']
+
+newsType['cultura'] = []
+
+newsType['horoscop'] = []
+
+newsType['IT&C'] = []
+
+newsType['Auto'] = []
+
+newsType['Monden'] = []
+
